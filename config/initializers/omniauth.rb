9e7491ad8e195194f2c74ed281ca446921ccce47
Arpit Kulshrestha <Arpit_Kulshrestha@external.mckinsey.com>
2016-10-23 04:08:33 +0530

    SSL cert

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1097370130305460', 'd0333330f534fbd79848c607a62aee57',
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
       {:scope => 'email,user_birthday,user_location,user_actions.music,public_profile,user_friends',
       :info_fields => 'id, name, first_name, middle_name,last_name, age_range, link, gender, locale, timezone, updated_time, verified, email, birthday, location', :display => 'popup',:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
